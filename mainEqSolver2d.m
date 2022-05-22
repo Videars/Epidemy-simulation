fe21f74f0fdc9acc576260da20a0eae246c35c55
Videars <105921751+Videars@users.noreply.github.com>
2022-05-22 12:53:04 +0200

    Update mainEqSolver2d.m

--- mainEqSolver2d.m
+++ mainEqSolver2d.m
@@ -9,15 +9,17 @@
 % Jacobian of f
  J = @(xy) [1 - Lam*xy(2), Lam*(1-xy(1)); 
             Lam*(1-xy(2)), 1 - Lam*xy(1)];
-        
+% Jacobian_inverse of f
+ Jinv = @(xy) (1/((1+Lam*xy(2))*(1+Lam*xy(1))-(Lam)^2*(1-xy(1))*(1-xy(2))))*[(-1-Lam*xy(1)), (Lam*(xy(1)-1)); 
+            (Lam*(xy(2)-1)), (-1-Lam*xy(2))];
 % Initial guesses
-xy = [0.5; 0.5];
-xy = newton2d(f,J,xy);
+xy = [0.8; 0.8];
+xy = newton2d(f,Jinv,xy);
 
 x = xy(1); y = xy(2);
 
 disp('zeroes values:')
 disp(['x = ',num2str(x), '  y = ', num2str(y)])
 
-disp('function values:' )
+disp('function values:')
 disp(f(xy))
