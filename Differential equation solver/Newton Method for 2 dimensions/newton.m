a38cf591f81fe131f56df457f30fcb0a7c2b0e3d
Videars <105921751+Videars@users.noreply.github.com>
2022-06-17 13:14:02 +0200

    Update newton.m

--- Differential equation solver/Newton Method for 2 dimensions/newton.m
+++ Differential equation solver/Newton Method for 2 dimensions/newton.m
@@ -4,10 +4,8 @@ function Xi = newton(f,Jinv,Xi)
     err = 1;
     maxits = 100;
     its = 0;
-    syms x1 x2 x3 x4 x5 x6 x7 x8 x9 x10
-    X = [x1; x2; x3; x4; x5; x6; x7; x8; x9; x10];
-%     syms x1 x2 x3 x4 x5
-%     X = [x1; x2; x3; x4; x5];
+    syms x1 x2
+    X = [x1; x2];
     
     while (err>tol) && (its<maxits)
          Xold = Xi;
